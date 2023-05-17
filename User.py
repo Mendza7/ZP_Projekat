099575465b407928951b1c205345bbfb70a20ed2
Mehmed <mendza1996@gmail.com>
2023-05-17 19:09:06 +0200

    User

---
+++ User.py
@@ -0,0 +1,11 @@
+class User:
+    def __init__(self, user_id, public_key, private_key):
+        self.timestamp = time.time()
+        self.user_id = user_id
+        self.public_key = public_key
+        self.private_key = private_key
+        self.key_id = self.generate_key_id(public_key)
+
+
+    def generate_key_id(self, public_key):
+        return hash(public_key) & 0xffffffffffffffff    
