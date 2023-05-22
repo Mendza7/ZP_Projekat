45e23c1a787ba6f5ee4a934b579d22627f24d9d3
Mehmed <mendza1996@gmail.com>
2023-05-22 21:14:55 +0200

    add encryptors/decryptors in user

--- User.py
+++ User.py
@@ -1,13 +1,43 @@
 import time
+import bcrypt
+from cryptography.hazmat.primitives import serialization
+from cryptography.hazmat.primitives.asymmetric import rsa
+
+from auth.DSSEncryptorDecryptor import DSSEncryptorDecryptor
+from auth.RSAEncryptorDecryptor import RSAEncryptorDecryptor
 
 
 class User:
-    def __init__(self, user_id, public_key, private_key):
+    def __init__(self, user_id, name, email=None, algorithm=None, key_size=1024, password=None):
+        self.password_hash = self.generate_password_hash(password)
         self.timestamp = time.time()
         self.user_id = user_id
-        self.public_key = public_key
-        self.private_key = private_key
-        self.key_id = self.generate_key_id(public_key)
+        self.name=name
+        self.email=email
+        self.algorithm=algorithm
+        self.authService  = self.generate_key_pair(algorithm,key_size)
+        self.key_id = self.generate_key_id(self.public_key)
 
-    def generate_key_id(self, public_key):
-        return hash(public_key) & 0xffffffffffffffff
+    def generate_password_hash(self, password):
+        salt = bcrypt.gensalt()
+        password_hash = bcrypt.hashpw(password.encode(), salt)
+        return password_hash
+
+    def verify_password(self, entered_password):
+        entered_password_hash = bcrypt.hashpw(entered_password.encode(), self.password_hash)
+        return bcrypt.checkpw(entered_password.encode(), entered_password_hash)
+
+    def generate_key_pair(self,algorithm, key_size):
+        if algorithm=='rsa':
+            return RSAEncryptorDecryptor(key_size)
+        elif algorithm=='dsa':
+            return DSSEncryptorDecryptor(key_size)
+
+
+
+    @staticmethod
+    def generate_key_id(public_key):
+        # Generate a key ID using the hash of the public key
+        key_id = hash(public_key) & 0xffffffffffffffff
+        return key_id
+
